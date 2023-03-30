eea079767c2c0d8c990057491a0bfd9fbccf986a
Noob_Park <sobit123@naver.com>
2023-03-30 20:15:54 +0900

    html, css almost done

--- src/components/worldcup.tsx
+++ src/components/worldcup.tsx
@@ -4,7 +4,17 @@ export const Worldcup = ():JSX.Element => {
     return (
         <>
             <div className="container">
-                <h1 className="title">원신 이상형 월드컵</h1>
+                <div className="title_container">
+                    <h1 className="title">원신 이상형 월드컵</h1>
+                </div>
+                <div className="contents_container">
+                    <div className="contents_A">
+                        <h1 className="character_name_A">앰버</h1>
+                    </div>
+                    <div className="contents_B">
+                        <h1 className="character_name_B">리사</h1>
+                    </div>
+                </div>
             </div>
         </>
     )
