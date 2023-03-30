f36deabba45726fdf68628a1321b8077fc1bad9c
Noob_Park <sobit123@naver.com>
2023-03-30 17:00:35 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Worldcup } from "./components/worldcup"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Worldcup/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/worldcup.tsx
@@ -0,0 +1,11 @@
+import "./worldcup.css";
+
+export const Worldcup = ():JSX.Element => {
+    return (
+        <>
+            <div className="container">
+                <h1 className="title">원신 이상형 월드컵</h1>
+            </div>
+        </>
+    )
+}
