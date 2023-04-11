3b6c3063f20da24f032735b48dea7d8ad9da2a7a
Noob_Park <sobit123@naver.com>
2023-04-11 20:59:55 +0900

    count modal

---
+++ src/components/countinput.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react"
+
+type CountInputProps = {
+    count : number
+} 
+
+export const CountInput = ():ReactElement => {
+    return (
+        <>
+            <div style={{ fontSize : "10rem" }}>테스트중</div>
+        </>
+    )
+}
--- src/components/worldcup.tsx
+++ src/components/worldcup.tsx
@@ -1,8 +1,17 @@
+import { CountInput } from "./countinput";
 import "./worldcup.css";
+import { useState, useEffect, ReactElement } from "react";
 
-export const Worldcup = ():JSX.Element => {
+export const Worldcup = () => {
+
+    const [count, setCount] = useState<number>(0);
+    const [countModal, setCountModal] = useState<boolean>(true);
+
     return (
         <>
+        {/* ▼ 몇강으로 할지 선택 */}
+        { countModal === true ? <CountInput/> : null }
+        {/* ▲ 몇강으로 할지 선택 */}
             <div className="container">
                 <div className="title_container">
                     <h1 className="title">원신 이상형 월드컵</h1>
@@ -10,9 +19,11 @@ export const Worldcup = ():JSX.Element => {
                 <div className="contents_container">
                     <div className="contents_A">
                         <h1 className="character_name_A">앰버</h1>
+                        <div className="character_img_A" style={ { backgroundImage : "url('src/assets/hodu.png')" } }></div>
                     </div>
                     <div className="contents_B">
                         <h1 className="character_name_B">리사</h1>
+                        <div className="character_img_B" style={ {  } }></div>
                     </div>
                 </div>
             </div>
